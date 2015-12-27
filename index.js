7a70c200c2d409f4ea2959a7eb01b4eb108f465b
rav009 <rav009_student@sina.com>
2015-12-27 19:25:18 +0800

    v1.0

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-aa =function(w){
-	alert(w)
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('save').addEventListener('click', save);
+	document.getElementById('password').addEventListener('click',redirect)
+	document.getElementById('port').value = localStorage.getItem('fifa1qport')
+	document.getElementById('password').value = localStorage.getItem('fifa1qpassword')
+});
+
+function save(){
+	if(!window.localStorage){
+    		alert('浏览器不支持localStorage!');
+	}
+	var port = document.getElementById('port').value
+	var password=document.getElementById('password').value
+	//alert(port)
+	//alert(password)
+	localStorage.setItem('fifa1qport',port);
+	localStorage.setItem('fifa1qpassword',password);
 }
 
+function redirect(){
+	var port = document.getElementById('port').value
+	var password=document.getElementById('password').value
+}
 
-var init = funtion(){
-	if(window.localStorage){
-    	alert('你的浏览器支持localStorage!');
-	}else{
-    	alert('浏览器不支持localStorage!');
-	}
-};
-init();
